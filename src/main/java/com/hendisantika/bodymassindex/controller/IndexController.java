2a7d1357bb7bdd970b5dcf08fc570bed54b98698
hendisantika <hendisantika@yahoo.co.id>
2024-01-08 15:59:33 +0700

    Add IndexController 🫸🌀✏️📗 :octocat:🐧🐳⬆

---
+++ src/main/java/com/hendisantika/bodymassindex/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.hendisantika.bodymassindex.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : body-mass-index
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/8/24
+ * Time: 15:59
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+@RequestMapping
+public class IndexController {
+}
