0330c48d110e89a83cb6932291bc802ff513a519
hendisantika <hendisantika@yahoo.co.id>
2024-01-08 15:57:04 +0700

    Add WebConfig 🫸🌀✏️📗 :octocat:🐧🐳⬆

---
+++ src/main/java/com/hendisantika/bodymassindex/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.hendisantika.bodymassindex.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : body-mass-index
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/8/24
+ * Time: 15:56
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+}
