2b6789d76f3915fffc29ad69b312c1bf85ee12fb
hendisantika <hendisantika@yahoo.co.id>
2024-01-08 15:59:57 +0700

    Add indexPage in IndexController 🫸🌀✏️📗 :octocat:🐧🐳⬆

--- src/main/java/com/hendisantika/bodymassindex/controller/IndexController.java
+++ src/main/java/com/hendisantika/bodymassindex/controller/IndexController.java
@@ -1,6 +1,9 @@
 package com.hendisantika.bodymassindex.controller;
 
+import com.hendisantika.bodymassindex.dto.UserInfoDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -16,4 +19,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping
 public class IndexController {
+    @GetMapping("/")
+    public String indexPage(Model model) {
+        UserInfoDto userInfoDto = new UserInfoDto();
+        userInfoDto.setHeight(40d).setWeight(2d);
+        model.addAttribute("userInfo", userInfoDto);
+
+        return "index";
+    }
 }
