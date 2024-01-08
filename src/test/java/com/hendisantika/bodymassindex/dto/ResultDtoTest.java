0ff0e0d5a35d2f0cbecb438ae46b713a0bc542a1
hendisantika <hendisantika@yahoo.co.id>
2024-01-08 16:00:56 +0700

    Add ResultDtoTest🫸🌀✏️📗 :octocat:🐧🐳⬆

---
+++ src/test/java/com/hendisantika/bodymassindex/dto/ResultDtoTest.java
@@ -0,0 +1,15 @@
+package com.hendisantika.bodymassindex.dto;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : body-mass-index
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/8/24
+ * Time: 16:00
+ * To change this template use File | Settings | File Templates.
+ */
+class ResultDtoTest {
+
+}
